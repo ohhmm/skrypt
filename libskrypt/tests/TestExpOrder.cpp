0065b07c1d6ab9c9852b7e5ba62983e8e14de022
Serg <sergeikrivonos@gmail.com>
2024-03-01 15:44:29 +0100

    Add test for mathimatical expression operator order

---
+++ libskrypt/tests/TestExpOrder.cpp
@@ -0,0 +1,34 @@
+#define BOOST_TEST_MODULE skryptExpOrder test
+#include <boost/test/unit_test.hpp>
+#include "skrypt.h"
+
+using namespace ::skrypt;
+using namespace std::string_literals;
+using namespace std::string_view_literals;
+
+BOOST_AUTO_TEST_CASE(skryptExpOrdertest){
+    Skrypt skryptWithBrackets;
+    skryptWithBrackets.Load(TEST_SRC_DIR "TestExpOrderWithBrackets.skrypt");
+
+    // get the variable
+    auto variableName = "x"s;
+    auto varhost = skryptWithBrackets.GetVarHost();
+    auto& v = varhost->Host(variableName);
+
+    // check that the variable is already solved
+    auto solutions = skryptWithBrackets.Known(v);
+    BOOST_TEST(solutions.size() == 1);
+    for (auto& solution : solutions) {
+        BOOST_TEST(solution == 1);
+    }
+
+
+    Skrypt skryptWithExpNoBrackets;
+    skryptWithExpNoBrackets.Load(TEST_SRC_DIR "TestExpBracketless.skrypt");
+    auto solutionsWithExpNoBrackets = skryptWithExpNoBrackets.Known(skryptWithExpNoBrackets.GetVarHost()->Host(variableName));
+    BOOST_TEST(solutionsWithExpNoBrackets.size() == 1);
+    for (auto& solution : solutionsWithExpNoBrackets) {
+        BOOST_TEST(solution == 1);
+    }
+    BOOST_TEST(solutionsWithExpNoBrackets == solutions);
+}
